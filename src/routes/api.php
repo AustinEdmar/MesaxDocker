44dde6c09663ec7d2d069841b8a71ba3626410cf
Austin Edmar <domingosmussumar@gmail.com>
2026-04-17 16:12:57 +0100

    rotas

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\TableController;
 use App\Http\Controllers\Api\ShiftsController;
@@ -10,56 +9,61 @@ use App\Http\Controllers\Api\CategoryController;
 
 use Illuminate\Support\Facades\Route;
 
-/* Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum'); */
-
 Route::get('/teste', function () {
     return 'teste';
 });
 
-
-//Route::get('/', [App\Http\Controllers\Api\AuthController::class, 'index']);
-
-//Route::get('/', [AuthController::class, 'index']);
+// Auth (público)
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 
-
 Route::middleware('auth:sanctum')->group(function () {
 
-    Route::apiResource('users', AuthController::class);
-    Route::apiResource('tables', TableController::class);
+    // Auth
     Route::get('/user', [AuthController::class, 'user']);
     Route::post('/logout', [AuthController::class, 'logout']);
 
-    //shifts
+    // Users
+    Route::get('/users', [AuthController::class, 'index']);
+    Route::put('/users/{user}', [AuthController::class, 'update']);
+
+    // Tables
+    Route::get('/tables', [TableController::class, 'index']);
+    Route::post('/tables', [TableController::class, 'store']);
+    Route::get('/tables/{table}', [TableController::class, 'show']);
+    Route::put('/tables/{table}', [TableController::class, 'update']);
+    Route::delete('/tables/{table}', [TableController::class, 'destroy']);
+
+    // Shifts
     Route::get('/shifts/current', [ShiftsController::class, 'current']);
     Route::post('/shifts/open', [ShiftsController::class, 'open']);
     Route::post('/shifts/close', [ShiftsController::class, 'close']);
 
+    // Products
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::get('/products/{product}', [ProductController::class, 'show']);
+    Route::put('/products/{product}', [ProductController::class, 'update']);
+    Route::delete('/products/{product}', [ProductController::class, 'destroy']);
 
-    //  Route::patch('/users/{user}', [AuthController::class, 'updateUser']); ok
+    // Categories
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::get('/categories/{category}', [CategoryController::class, 'show']);
+    Route::put('/categories/{category}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{category}', [CategoryController::class, 'destroy']);
 
-    // Route::post('/update-user', [AuthController::class, 'updateUser']);
-
-    //Products
-    Route::apiResource('products', ProductController::class);
-
-    //Categories
-    Route::apiResource('categories', CategoryController::class);
-
-    //Orders
-    Route::post('/orders/open', [OrderController::class, 'open']);
+    // Orders
     Route::get('/orders', [OrderController::class, 'getOrders']);
+    Route::post('/orders/open', [OrderController::class, 'open']);
     Route::post('/orders/{id}/add-item', [OrderController::class, 'addItem']);
     Route::post('/orders/{id}/decrement-item', [OrderController::class, 'decrementItem']);
     Route::delete('/orders/item/{id}', [OrderController::class, 'removeItem']);
     Route::post('/orders/{id}/close', [OrderController::class, 'close']);
 
-    // Admin routes
+    // Admin
     Route::middleware('access.level:1')->group(function () {
         Route::get('/admin/users', [AuthController::class, 'listUsers']);
         Route::put('/admin/users/{user}', [AuthController::class, 'updateUser']);
