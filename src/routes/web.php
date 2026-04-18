0d18ba7b6edcf8099fb519c2f2f3ac27e3019eaf
Austin Edmar <domingosmussumar@gmail.com>
2026-04-18 22:24:26 +0100

    deploy

--- src/routes/web.php
+++ src/routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 
@@ -31,9 +31,9 @@ Route::get('/chat', function () {
 Route::post('/send-message', function (Request $request) {
   $user = $request->input('user', 'Usuário Anônimo');
   $message = $request->input('message');
-  
+
   event(new PublicMessage($user, $message));
-  
+
   return response()->json(['status' => 'Mensagem enviada']);
 });
 
@@ -41,27 +41,3 @@ Route::post('/send-message', function (Request $request) {
 
 Auth::routes();
 
-
-Route::middleware(['auth'])->group(function () {
-   // Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
-
-
-
- Route::resource('tables', TableController::class);
- Route::resource('category', CategoryController::class);
- Route::resource('sub_categories', SubCategoryController::class);
- Route::resource('type_categories', TypeCategoryController::class);
- Route::resource('products', ProductController::class);
- Route::resource('stocks', StockController::class);
-
-
- Route::resource('cuisines', CuisineController::class);
- Route::resource('categories', DishesCategoryController::class);
- Route::resource('subcategories', SubcategoryController::class);
- Route::resource('dishes', DishesController::class);
- Route::resource('sides', SidesController::class);
- Route::resource('sauces', SaucesController::class);
- 
-
-
-});
